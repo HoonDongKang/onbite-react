f93c36d51d58b566233bdfe1526c40f087570489
Donghoon Kang <d159123@naver.com>
2024-07-21 21:48:11 +0900

    feat: Add useState

--- section04/src/App.jsx
+++ section04/src/App.jsx
@@ -1,14 +1,21 @@
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-
+import { useState } from "react";
 function App() {
+  // [현재값, 상태변화 함수값] = useState(초기값)
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("OFF");
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <div>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </div>
+      <div>
+        <h1>{light}</h1>
+        <button onClick={() => setLight(light === "ON" ? "OFF" : "ON")}>
+          전구 {light === "ON" ? "끄기" : "켜기"}
+        </button>
+      </div>
     </>
   );
 }
