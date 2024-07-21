8fbf36c803eca6167d5f475ba5c6bd8c448e743a
Donghoon Kang <d159123@naver.com>
2024-07-21 21:37:41 +0900

    feat: Add Event handler

--- section04/src/components/Buttons.jsx
+++ section04/src/components/Buttons.jsx
@@ -1,6 +1,14 @@
 const Button = ({ text, color, children }) => {
+  const onClickBtn = (e) => {
+    //syntheticBaseEvent : 합성 이벤트
+    // 모든 웹페이지의 이벤트를 통합시킨 형태
+    //<-> Cross Browsing Issue : 서로 다른 브라우저의 이벤트로 인한 에러
+    console.log(e);
+    console.log(text);
+  };
+
   return (
-    <button style={{ color }}>
+    <button onClick={onClickBtn} style={{ color }}>
       {text}
       {children}
     </button>
