c27601bc90532edfa14433969da9897c0c0c8778
Donghoon Kang <d159123@naver.com>
2024-07-22 23:24:03 +0900

    Bulb component

--- section04/src/App.jsx
+++ section04/src/App.jsx
@@ -1,5 +1,17 @@
 import "./App.css";
 import { useState } from "react";
+const Bulb = ({ light }) => {
+  return (
+    <div>
+      {light === "ON" ? (
+        <h1 style={{ backgroundColor: "orange" }}>ON</h1>
+      ) : (
+        <h1 style={{ backgroundColor: "gray" }}>OFF</h1>
+      )}
+    </div>
+  );
+};
+
 function App() {
   // [현재값, 상태변화 함수값] = useState(초기값)
   const [count, setCount] = useState(0);
@@ -11,7 +23,7 @@ function App() {
         <button onClick={() => setCount(count + 1)}>+</button>
       </div>
       <div>
-        <h1>{light}</h1>
+        <Bulb light={light} />
         <button onClick={() => setLight(light === "ON" ? "OFF" : "ON")}>
           전구 {light === "ON" ? "끄기" : "켜기"}
         </button>
