b60b4e985ff1ba6f882b3c666266b51d456e0e64
Donghoon Kang <d159123@naver.com>
2024-07-24 23:09:10 +0900

    feat: Add input tags

--- section04/src/App.jsx
+++ section04/src/App.jsx
@@ -1,14 +1,9 @@
 import "./App.css";
-import Bulb from "./components/Bulb";
-import Counter from "./components/Counter";
-
+import Register from "./components/Register";
 function App() {
-  // [현재값, 상태변화 함수값] = useState(초기값)
-
   return (
     <>
-      <Bulb />
-      <Counter />
+      <Register />
     </>
   );
 }
