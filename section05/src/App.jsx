3838c9f74c6627eac553c8135b44117dd61aee04
Donghoon Kang <d159123@naver.com>
2024-08-10 23:53:20 +0900

    feat: add viewer

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Controller";
 
 function App() {
-  return <>카운트앱</>;
+  return (
+    <>
+      <Viewer />
+      <Controller />
+    </>
+  );
 }
 
 export default App;
