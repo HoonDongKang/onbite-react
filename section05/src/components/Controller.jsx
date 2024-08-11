7e6155a3a73c3254a94b900c841d8c1c9869b40a
Donghoon Kang <d159123@naver.com>
2024-08-11 20:25:09 +0900

    feat: Add counter

--- section05/src/components/Controller.jsx
+++ section05/src/components/Controller.jsx
@@ -1,12 +1,20 @@
-const Controller = () => {
+const Controller = ({ onClickButton }) => {
+  const CONTROL_VALUES = [
+    { text: "-1", value: -1 },
+    { text: "-10", value: -10 },
+    { text: "-100", value: -100 },
+    { text: "+100", value: 100 },
+    { text: "+10", value: 10 },
+    { text: "+1", value: 1 },
+  ];
+
   return (
     <div>
-      <button>-1</button>
-      <button>-10</button>
-      <button>-100</button>
-      <button>+100</button>
-      <button>+10</button>
-      <button>+1</button>
+      {CONTROL_VALUES.map((item, index) => (
+        <button key={index} onClick={() => onClickButton(item.value)}>
+          {item.text}
+        </button>
+      ))}
     </div>
   );
 };
