9f03017080dd95d6bb56d94a0429fc70fd526dbd
Donghoon Kang <d159123@naver.com>
2024-08-15 22:02:09 +0900

    feat: lifecycl with useeffect

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,22 +1,39 @@
 import "./App.css";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import Even from "./components/Even";
 
 function App() {
   const [count, setCount] = useState(0);
+  const isMount = useRef(false);
 
-  useEffect(() => {
-    console.log("count:" + count);
-  }, [count]);
   const onClickButton = (value) => {
     setCount(count + value);
   };
+
+  //1. mount : 탄생
+  useEffect(() => {
+    console.log("mount");
+  }, []);
+
+  //2. update : 변화, 리렌더링
+  useEffect(() => {
+    if (!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  });
+
+  //3. unmount : 죽음
+
   return (
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
       <section>
         <Controller onClickButton={onClickButton} />
